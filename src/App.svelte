84a869f19f6990dd343497710c3ff2cb880c3cba
madarsbiss <madars.biss@gmail.com>
2020-11-17 21:00:25 +0200

    added reset, levels and field comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,65 @@
-<script>
-  export let name;
-</script>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  .game-frame {
-    width: 600px;
-    height: 600px;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  .field {
-    background-color: bisque;
-    border: 1px solid grey;
-    cursor: pointer;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
 <main>
-  <h1>Tic Tac Toe</h1>
-  <div class="game-frame">
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-    <div class="field" />
-  </div>
+   <h1>Tic Tac Toe</h1>
+   <label for="level">Select a level:</label>
+   <select name="level" id="level">
+      <option value="">Beginner</option>
+      <option value="saab">Medium</option>
+      <option value="mercedes">Hard</option>
+   </select>
+
+   <div class="game-frame">
+      {#each Array(9) as field, index}
+    <Field onClick={()=>{console.log('er4t34regrfs')}} />
+{/each}
+   </div>
+   <h3>New game</h3>
 </main>
+
+<script>
+import Field from './Field.svelte'
+
+let winning = [
+   [1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9],
+   [1, 4, 7],
+   [2, 5, 8],
+   [3, 6, 9],
+   [1, 5, 9],
+   [3, 5, 7]
+]
+
+const winner = () => {
+   console.log('Checking for winner');
+}
+</script>
+<style>
+main {
+   text-align: center;
+   padding: 1em;
+   max-width: 240px;
+   margin: 0 auto;
+}
+
+.game-frame {
+   width: 400px;
+   height: 400px;
+   margin: 0 auto;
+   display: grid;
+   grid-template-columns: repeat(3, 1fr);
+   gap: 10px;
+}
+
+h1 {
+   color: #ff3e00;
+   text-transform: uppercase;
+   font-size: 4em;
+   font-weight: 100;
+}
+
+@media (min-width: 640px) {
+   main {
+      max-width: none;
+   }
+}
+</style>
---
+++ src/Field.svelte
@@ -0,0 +1,20 @@
+<div class="field" onClick={onClick} />
+
+<script>
+export let onClick;
+</script>
+
+<style>
+
+.field {
+   background-color: rgb(203, 255, 196);
+   border: 1px solid rgb(65, 161, 0);
+   cursor: pointer;
+   border-radius: 20px;
+}
+
+.field:hover {
+   background-color: rgb(182, 255, 173);
+   border: 1px solid rgb(38, 95, 0)
+}
+</style>
