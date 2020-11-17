08d6a6de3611f30bfe2cecd9e1b190da54693654
madarsbiss <madars.biss@gmail.com>
2020-11-17 22:26:02 +0200

    user nad pc moves

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,99 @@
-<main>
-   <h1>Tic Tac Toe</h1>
-   <label for="level">Select a level:</label>
-   <select name="level" id="level">
-      <option value="">Beginner</option>
-      <option value="saab">Medium</option>
-      <option value="mercedes">Hard</option>
-   </select>
-
-   <div class="game-frame">
-      {#each Array(9) as field, index}
-    <Field onClick={()=>{console.log('er4t34regrfs')}} />
-{/each}
-   </div>
-   <h3>New game</h3>
-</main>
-
 <script>
-import Field from './Field.svelte'
+  import Field from "./Field.svelte";
 
-let winning = [
-   [1, 2, 3],
-   [4, 5, 6],
-   [7, 8, 9],
-   [1, 4, 7],
-   [2, 5, 8],
-   [3, 6, 9],
-   [1, 5, 9],
-   [3, 5, 7]
-]
+  let winning = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
 
-const winner = () => {
-   console.log('Checking for winner');
-}
+  let moves = 0;
+  let arr = [];
+
+  const winner = () => {
+    console.log("Checking for winner");
+    if (moves == 9) {
+    }
+  };
+
+  const userMove = (e) => {
+    e.target.innerHTML = "X";
+    ++moves;
+    arr.push(parseInt(e.target.getAttribute("number")));
+    console.log(arr);
+    if (moves !== 9) {
+      pcMove();
+    } else {
+      winner();
+    }
+  };
+
+  const pcMove = () => {
+    // pc generates rand number from 0 to 8
+    let num;
+
+    do {
+      num = Math.floor(Math.random() * 9);
+      console.log(num);
+    } while (arr.includes(num));
+
+    const game = document.querySelector(".game-frame");
+
+    game.childNodes[num].innerHTML = "O";
+    arr.push(num);
+    ++moves;
+  };
 </script>
+
 <style>
-main {
-   text-align: center;
-   padding: 1em;
-   max-width: 240px;
-   margin: 0 auto;
-}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-.game-frame {
-   width: 400px;
-   height: 400px;
-   margin: 0 auto;
-   display: grid;
-   grid-template-columns: repeat(3, 1fr);
-   gap: 10px;
-}
+  .game-frame {
+    width: 400px;
+    height: 400px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 10px;
+  }
 
-h1 {
-   color: #ff3e00;
-   text-transform: uppercase;
-   font-size: 4em;
-   font-weight: 100;
-}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-@media (min-width: 640px) {
-   main {
+  @media (min-width: 640px) {
+    main {
       max-width: none;
-   }
-}
+    }
+  }
 </style>
+
+<main>
+  <h1>Tic Tac Toe</h1>
+  <label for="level">Select a level:</label>
+  <select name="level" id="level">
+    <option value="">Beginner</option>
+    <option value="saab">Medium</option>
+    <option value="mercedes">Hard</option>
+  </select>
+
+  <div class="game-frame">
+    {#each Array(9) as field, index}
+      <Field onClick={userMove} number={index} />
+    {/each}
+  </div>
+  <h3>New game</h3>
+</main>
--- src/Field.svelte
+++ src/Field.svelte
@@ -1,20 +1,19 @@
-<div class="field" onClick={onClick} />
-
 <script>
-export let onClick;
+  export let onClick, number;
 </script>
 
 <style>
+  .field {
+    background-color: rgb(203, 255, 196);
+    border: 1px solid rgb(65, 161, 0);
+    cursor: pointer;
+    border-radius: 20px;
+  }
 
-.field {
-   background-color: rgb(203, 255, 196);
-   border: 1px solid rgb(65, 161, 0);
-   cursor: pointer;
-   border-radius: 20px;
-}
-
-.field:hover {
-   background-color: rgb(182, 255, 173);
-   border: 1px solid rgb(38, 95, 0)
-}
+  .field:hover {
+    background-color: rgb(182, 255, 173);
+    border: 1px solid rgb(38, 95, 0);
+  }
 </style>
+
+<div class="field" on:click={onClick} {number} />
